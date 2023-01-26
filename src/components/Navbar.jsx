c3aa053bb68feb3aad25c4383da9eb2be25c22c4
mvlopesm <mvlm28797@gmail.com>
2023-01-25 21:25:23 -0300

    estrutura e estilização do header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import imgMarcaTaugor from '../assets/marca-taugor.png'
+import { AiFillHome } from "react-icons/ai";
+
+import './Navbar.css'
 
 const NavBar = () => {
     return (
         <nav id='navbar'>
-            <Link to='/'>
-                <img src={imgMarcaTaugor} alt="" />
-            </Link>
-            <Link to='/meus-funcionarios'>Meus Funcionário</Link>
-            <Link to='/atualizar-cadastro'>Atualizar Cadastro</Link>
-            <Link to='cadastrar-funcionario'>Cadastrar Funcionário</Link>
+            <div className="logo">
+                <Link to='/'>
+                    <img src={imgMarcaTaugor} alt="" />
+                </Link>
+            </div>
+
+            <div className="links-nav">
+                <div>
+                    <Link to='/meus-funcionarios'>Meus Funcionário</Link>
+                </div>
+                <div>
+                    <Link to='/atualizar-cadastro'>Atualizar Cadastro</Link>
+                </div>
+                <div>
+                    <Link to='cadastrar-funcionario'>Cadastrar Funcionário</Link>
+                </div> 
+            </div>
+            
+            <div className="home-button">
+                <AiFillHome className="home-icon"/>
+            </div>
       </nav>
     )
 }
