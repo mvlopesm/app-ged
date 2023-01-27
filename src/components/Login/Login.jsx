29f28ee75a3c914a2c879a302954d9ecd17f5cf9
mvlopesm <mvlm28797@gmail.com>
2023-01-27 11:08:19 -0300

    implementação do firebase auth na tela de login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 // @ts-ignore
 import imgMarcaTaugor from '../../assets/marca-taugor.png'
-
 import './Login.css'
+
+import firebase from '../../../firebase';
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { dividerClasses } from "@mui/material";
 
 
 const Login = () => {
 
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [sucesso, setSucesso] = useState ('')
+
+  const auth = getAuth()
   const LoginUsuario = () => {
-    alert("login")
+    signInWithEmailAndPassword(auth, email, senha)
+      .then((userCredential) => {
+        setSucesso('S')
+      })
+      .catch((error) => {
+        setSucesso('N')
+        const errorCode = error.code;
+        const errorMessage = error.message
+    })
   }
+  
+
+  const alterarEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const alterarSenha = (event) => {
+    setSenha(event.target.value)
+}
 
   return (
       <div className="d-flex align-itens-center text-center form-container">
@@ -20,16 +46,18 @@ const Login = () => {
           <h1 className="h3 mb-3 fw-normal">Login</h1>
 
           <div className="">
-            <input type="email" className="form-control" id="floatingInput" placeholder="Email"/>
+            <input onChange={alterarEmail} type="email" className="form-control" id="floatingInput" placeholder="Email"/>
             <label htmlFor="floatingInput">E-mail</label>
           </div>
 
           <div className="">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
+            <input onChange={alterarSenha}type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
             <label htmlFor="floatingPassword">Senha</label>
           </div>
 
           <button onClick={LoginUsuario} className="w-100 btn btn-lg btn-primary" type="button"><Link to='/meusFuncionarios' className="submitButton">Acessar</Link></button>
+
+          { sucesso === 'N' ? <div className="alert alert-danger mt-3" role="alert">Email ou senha inválida</div> : null}
 
           <div className="links">
               <Link to="recuperarSenha" className="mx-2">Esqueci minha senha</Link>
