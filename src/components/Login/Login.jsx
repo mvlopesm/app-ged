f138a703c8f2dbd80073dd7ac11f757eebd341c7
mvlopesm <mvlm28797@gmail.com>
2023-01-27 12:56:39 -0300

    implementação do firebase auth na pagina de criar conta

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,38 +5,29 @@ import { Link } from "react-router-dom";
 import imgMarcaTaugor from '../../assets/marca-taugor.png'
 import './Login.css'
 
-import firebase from '../../../firebase';
 import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
-import { dividerClasses } from "@mui/material";
+import app from "../../../firebase";
+
 
 
 const Login = () => {
 
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [sucesso, setSucesso] = useState ('')
+  const [password, setPassword] = useState("");
+  const [sucess, setsucess] = useState ('')
 
-  const auth = getAuth()
+  const auth = getAuth(app)
   const LoginUsuario = () => {
-    signInWithEmailAndPassword(auth, email, senha)
-      .then((userCredential) => {
-        setSucesso('S')
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        setsucess('S')
+        alert('Login')
       })
       .catch((error) => {
-        setSucesso('N')
-        const errorCode = error.code;
-        const errorMessage = error.message
+        setsucess('N')
     })
   }
-  
-
-  const alterarEmail = (event) => {
-    setEmail(event.target.value)
-  }
-
-  const alterarSenha = (event) => {
-    setSenha(event.target.value)
-}
 
   return (
       <div className="d-flex align-itens-center text-center form-container">
@@ -46,18 +37,18 @@ const Login = () => {
           <h1 className="h3 mb-3 fw-normal">Login</h1>
 
           <div className="">
-            <input onChange={alterarEmail} type="email" className="form-control" id="floatingInput" placeholder="Email"/>
+            <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="floatingInput" placeholder="Email"/>
             <label htmlFor="floatingInput">E-mail</label>
           </div>
 
           <div className="">
-            <input onChange={alterarSenha}type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
+            <input onChange={(e) => setPassword(e.target.value)}type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
             <label htmlFor="floatingPassword">Senha</label>
           </div>
 
-          <button onClick={LoginUsuario} className="w-100 btn btn-lg btn-primary" type="button"><Link to='/meusFuncionarios' className="submitButton">Acessar</Link></button>
+          <button onClick={LoginUsuario} className="w-100 btn btn-lg btn-primary" type="button">Acessar</button>
 
-          { sucesso === 'N' ? <div className="alert alert-danger mt-3" role="alert">Email ou senha inválida</div> : null}
+          { sucess === 'N' ? <div className="alert alert-danger mt-3" role="alert">Email ou senha inválida</div> : null}
 
           <div className="links">
               <Link to="recuperarSenha" className="mx-2">Esqueci minha senha</Link>
--- src/pages/CriarConta.jsx
+++ src/pages/CriarConta.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 // @ts-ignore
 import imgMarcaTaugor from '../assets/marca-taugor.png';
 
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase";
+
 const CriarConta = () => {
 
-    const LoginUsuario = () => {
-      alert("login")
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState ('')
+
+    const cadastrarUsuario = () => {
+      setMessage('')
+      
+      if (!email || !password) {
+        setMessage('Preencha todos os campos')
+        return;
+      }
+      const auth = getAuth(app);
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          alert('Usuário cadastrado com sucesso')
+        })
+        .catch((error) => {
+          setMessage(error.message)
+          if (error.message === 'Firebase: Password should be at least 6 characters (auth/weak-password).') {
+            setMessage('A senha deve ter 6 caracteres ou mais')
+          } else if (error.message === 'Firebase: Error (auth/email-already-in-use).'){
+            setMessage('E-mail já está em uso')
+          } else if (error.message ==='Firebase: Error (auth/invalid-email).') {
+            setMessage('E-mail inválido')
+          } else {
+            setMessage('Erro ao criar a conta: ' + error.message)
+          }
+        });
+    };
+
   
     return (
         <div className="d-flex align-itens-center text-center form-container">
@@ -17,20 +47,22 @@ const CriarConta = () => {
             <img className="mb-4" src={imgMarcaTaugor} alt=""/>
             <h1 className="h3 mb-3 fw-normal">Criar Conta</h1>
   
-            <div className="">
-              <input type="email" className="form-control" id="floatingInput" placeholder="Email"/>
+            <div>
+              <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="floatingInput" placeholder="Email"/>
               <label htmlFor="floatingInput">E-mail</label>
             </div>
   
-            <div className="">
-              <input type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
+            <div>
+              <input onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" id="floatingPassword" placeholder="Senha"/>
               <label htmlFor="floatingPassword">Senha</label>
             </div>
   
-            <button onClick={LoginUsuario} className="w-100 btn btn-lg btn-primary" type="button"><Link to='/meusFuncionarios' className="submitButton">Enviar</Link></button>
-  
+            <button onClick={cadastrarUsuario} className="w-100 btn btn-lg btn-primary" type="button">Cadastrar</button>
+
+            { message.length > 0 ? <div className="alert alert-danger mt-3" role="alert">{message}</div> : null}
+
             <div className="links">
-                <Link to="/login" className="mx-2">Já tenho uma conta</Link>
+              <Link to="/login" className="mx-2">Já tenho uma conta</Link>
             </div>
           </form>
         </div>
